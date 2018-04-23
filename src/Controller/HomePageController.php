3d8dffd37075eac0e28ff9f79230cdecaf7c38ef
rmedard <00Derfine@5>
2018-04-23 18:08:11 +0200

    W10

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -9,26 +9,16 @@
 namespace Drupal\mfasiri_interface\Controller;
 
 
-use Drupal;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\node\Entity\Node;
 
 /**
  * Class HomePageController
  * @package Drupal\mfasiri_interface\Controller
  */
 class HomePageController extends ControllerBase {
-
     public function content() {
-        $interpreterPlatform = Node::load(8);
-        $tripsToAfrica = Node::load(9);
-        $interpreterPlatformTrans = Drupal::service('entity.repository')->getTranslationFromContext($interpreterPlatform);
-        $tripsToAfricaTrans = Drupal::service('entity.repository')->getTranslationFromContext($tripsToAfrica);
-        $element = array(
-            '#theme' => 'mfasiri_homepage',
-            '#node1' => $interpreterPlatformTrans,
-            '#node2' => $tripsToAfricaTrans,
+        return array(
+            '#theme' => 'homepage'
         );
-        return $element;
     }
 }
---
+++ src/Plugin/Block/LanguageSwitcher.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: reberme
+ * Date: 13/04/2018
+ * Time: 15:29
+ */
+
+namespace Drupal\mfasiri_interface\Plugin\Block;
+
+
+use Drupal;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Url;
+
+/**
+ * Class LanguageSwitcher
+ *
+ * @package Drupal\mfasiri_interface\Plugin\Block
+ *
+ * @Block(
+ *     id = "mfasiri_language_switcher",
+ *     admin_label = @Translation("Mfasiri Language Switcher block"),
+ *     category = @Translation("Custom Mfasiri Blocks")
+ * )
+ */
+class LanguageSwitcher extends BlockBase {
+
+    /**
+     * Builds and returns the renderable array for this block plugin.
+     *
+     * If a block should not be rendered because it has no content, then this
+     * method must also ensure to return no content: it must then only return an
+     * empty array, or an empty array with #cache set (with cacheability metadata
+     * indicating the circumstances for it being empty).
+     *
+     * @return array
+     *   A renderable array representing the content of the block.
+     *
+     * @see \Drupal\block\BlockViewBuilder
+     */
+    public function build() {
+//        $currentUrl = Url::fromRoute('<current>', array('language' => Drupal::languageManager()->getCurrentLanguage()));
+        $currentUrl = Url::fromRoute('<current>');
+        $languages = Drupal::languageManager()
+            ->getLanguageSwitchLinks(LanguageInterface::TYPE_INTERFACE, $currentUrl);
+        kint(['route' => $currentUrl->toString(), 'languages' => $languages]);
+        return [
+            '#theme' => 'language_switcher'
+        ];
+    }
+}
